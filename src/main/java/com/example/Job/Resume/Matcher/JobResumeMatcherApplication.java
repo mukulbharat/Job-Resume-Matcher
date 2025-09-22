50112d61da861694373f201b283e572da183cff8
mukulbharat <mukulbharat@outlook.com>
2025-09-22 13:53:11 +0530

    Initial Commit

---
+++ src/main/java/com/example/Job/Resume/Matcher/JobResumeMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.Job.Resume.Matcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobResumeMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobResumeMatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Job/Resume/Matcher/JobResumeMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Job.Resume.Matcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobResumeMatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
